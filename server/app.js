dfe32be01e72078b30b4c5ad1475c1f2febc4b59
userFbi <pawartushar2024.katargam@gmail.com>
2026-05-08 09:07:42 +0530

    env file created"

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require("cors");
@@ -5,7 +6,7 @@ const cors = require("cors");
 const app = express();
 
 // DB
-mongoose.connect('mongodb://localhost:27017/Tp')
+mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('DB CONNECTED ✅'))
   .catch(err => console.log(err));
 
@@ -20,8 +21,6 @@ const expenseRoutes = require("./routes/expense");
 const habitRoutes = require("./routes/habit");
 const noteRoutes = require("./routes/note");
 const dayflowRoutes = require("./routes/dayflow");
-
-
 
 app.use("/planner", plannerRouter);
 app.use("/vault", vaultRoutes);
@@ -41,7 +40,8 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`SERVER RUNNING ON ${PORT} 🚀`);
 });
