dc015aa9d0cdd950fae802623689a62a4097708d
BirDan1988 <birjudanak@gmail.com>
2026-07-14 08:45:32 +0530

    fixed problem

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 
 const JWT_SECRET = process.env.JWT_SECRET || 'dev-only-insecure-secret-change-me';
 
